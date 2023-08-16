545e025c707037e5fcc7b009497d53030f00fbb9
bigwaveBigwave <gb6ke3@naver.com>
2023-08-17 01:05:47 +0900

    실습 2. 상품 카테고리 메뉴의 사용자 입력값이 없는 경우 재입력 메시지 출력 처리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,5 +13,28 @@ fun main(args: Array<String>) {//fun이 함수이름이고, 메인함수
           원하시는 카테고리를 입력해주세요
      """.trimIndent()) //trimindent = 문장의 들여쓰기를 없애줌
 
+     /*
+    step1. 상품 카테고리 표기
+    step2. 사용자 입력 받기
+    step3. 사용자가 기대하지 않은 답을 입력한 경우 예외 처리
+     */
+     val categories = arrayOf("패션","전자기기","반려동물용품")
+     for(category in categories){
+          println(category)
+     }
+     println("=> 장바구니로 이동하시려면 #을 입력해주세요")
+
+     var selectedCategory = readLine()
+     while(selectedCategory.isNullOrBlank()){
+          println("값을 입력해주세요")
+          selectedCategory = readLine()
+     }
+     if(selectedCategory =="#"){
+          //TODO 1. 장바구니이동
+     }else{
+          //TODO 2. 카테고리 상품 목록 보여주기
+          //TODO 3. 카테에 없는 값 입력하는 경우
+     }
+
 
 }
